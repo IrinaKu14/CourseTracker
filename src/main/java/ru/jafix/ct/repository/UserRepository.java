2f2ef479555f74db6b0ba303b7e05618fbe99400
IrinaKu <irina.kutlatova@gmail.com>
2025-05-08 12:36:57 +0400

    Init commit Dto

--- src/main/java/ru/jafix/ct/repository/UserRepository.java
+++ src/main/java/ru/jafix/ct/repository/UserRepository.java
@@ -8,11 +8,15 @@ import org.springframework.stereotype.Repository;
 import ru.jafix.ct.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    User findByLogin(String login);
+
+    Optional<User> findByLogin(String login);
+
+
     List<User> findByAgeGreaterThan(int age);
 //    @Query(value = "SELECT * FROM users u WHERE u.user_age <?1 AND u.login like ?2", nativeQuery = true)
 //    List<User> findByLoginStartsWithLitterAndAgeBefore(@Param("maxAge") int maxAge,@Param("liter") String liter);
--- src/main/java/ru/jafix/ct/service/UserService.java
+++ src/main/java/ru/jafix/ct/service/UserService.java
@@ -6,6 +6,10 @@ import ru.jafix.ct.entity.User;
 import ru.jafix.ct.entity.dto.UserDto;
 import ru.jafix.ct.repository.UserRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 
 public class UserService {
@@ -13,8 +17,8 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User createUser(UserDto userDto){
-        if(userDto.getLogin() == null || userDto.getLogin().isEmpty() ){
+    public User createUser(UserDto userDto) {
+        if (userDto.getLogin() == null || userDto.getLogin().isEmpty()) {
             throw new IllegalArgumentException("login is required field");
         }
         User userForCreater = User.builder()
@@ -26,7 +30,7 @@ public class UserService {
     }
 
     //изменить пользователя
-    public User editUser(UserDto userDto){
+    public User editUser(UserDto userDto) {
         User userForCreater = User.builder()
                 .id(userDto.getId())
                 .age(userDto.getAge())
@@ -37,9 +41,39 @@ public class UserService {
     }
 
     //получить пользователя по id
+    public User findUserById(UUID id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isEmpty()) {
+            throw new IllegalArgumentException("Пользовател с таким id не существует");
+        }
+
+        return userOptional.get();
+    }
+
+    //аналогично
+    public User findUserById2(UUID id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Пользовател с таким id не существует"));
+    }
+//получить пользователя по login
+
+    public User findUserByLogin(String login) {
+        Optional<User> userOptional = userRepository.findByLogin(login);
+        if (userOptional.isEmpty()) {
+            throw new IllegalArgumentException("Пользовател с таким login не существует");
+        }
+
+        return userOptional.get();
+    }
 
     //получить список всех пользователей
+    public List<User> findAllUsers() {
 
+        return userRepository.findAll();
+    }
     //удалить пользователя по id
 
+    public void deleteById(UUID id) {
+        userRepository.deleteById(id);
+    }
 }

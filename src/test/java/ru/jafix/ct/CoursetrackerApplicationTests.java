62eb51964454fe1227f9c35b14978f5c40c849a3
IrinaKu <irina.kutlatova@gmail.com>
2025-05-26 23:52:55 +0400

    Test modul

--- src/test/java/ru/jafix/ct/CoursetrackerApplicationTests.java
+++ src/test/java/ru/jafix/ct/CoursetrackerApplicationTests.java
@@ -1,13 +1,22 @@
 package ru.jafix.ct;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 class CoursetrackerApplicationTests {
+//	@Autowired
+//	private Environment environment;
 
 	@Test
 	void contextLoads() {
+		//System.out.println(environment.getProperty("test.prop"));
 	}
 
 }
